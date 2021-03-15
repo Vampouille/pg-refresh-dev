2489cbb156c72e6aea1caa62b7c7f1b8201607ff
Julien Acroute <julien.acroute@camptocamp.com>
2021-03-15 23:53:38 +0100

    Create assets folder

--- server.py
+++ server.py
@@ -8,10 +8,9 @@ tasks = {}
 app = Flask(__name__)
 
 
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('js', path)
-
+@app.route('/assets/<path:path>')
+def send_assets(path):
+    return send_from_directory('assets', path)
 
 @app.route('/')
 def home():
