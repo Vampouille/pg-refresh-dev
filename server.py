310e67357120a0bd6eb43c6010a0fc5719a4d78c
Julien Acroute <julien.acroute@camptocamp.com>
2021-03-15 23:43:17 +0100

    Add more api endpoint and task id

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 import concurrent.futures
-import time
 from flask import Flask, render_template, send_from_directory, jsonify
 
 from task import Task
 
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-tasks = []
+tasks = {}
 app = Flask(__name__)
 
 
@@ -15,22 +14,21 @@ def send_js(path):
 
 
 @app.route('/')
-def hello_world():
-    return render_template('tasks.html')
+def home():
+    return render_template('index.html')
 
+@app.route('/api/task/', methods=['GET'])
+def tasks_list():
+    return jsonify([t.to_dict() for t in tasks.values() ])
 
 @app.route('/refresh')
-def refresh():
-    
-    tasks.append(Task(executor.submit(do_refresh, 'PROD', 'DEV'), 'PROD', 'DEV'))
-    return 'New task running'
+@app.route('/api/task/', methods=['POST'])
+def create_task():
+    task = Task(executor, 'PROD', 'DEV') 
+    tasks[task.id] = task
+    return jsonify(task.to_dict())
 
 
-def do_refresh(db_from, db_to):
-    time.sleep(30)
-    return 'OK: %s -> %s' % (db_from, db_to)
-  
-
-@app.route('/api/task/')
-def tasks_list():
-    return jsonify([t.to_dict() for t in tasks])
+@app.route('/api/task/<id>', methods=['GET'])
+def get_task(id):
+    return jsonify(tasks[id])
--- task.py
+++ task.py
@@ -1,14 +1,20 @@
 from concurrent.futures import Future
+import uuid
+import time
+
 
 class Task():
 
-    def __init__(self, task: Future, db_from: str, db_to: str):
-        self.future = task
+    def __init__(self, executor, db_from: str, db_to: str):
+        self.id = str(uuid.uuid1())
+        self.future = executor.submit(self.run)
         self.db_from = db_from
         self.db_to = db_to
+
     
     def to_dict(self):
-        return {'state': self.state(),
+        return {'id': self.id,
+                'state': self.state(),
                 'db_from': self.db_from,
                 'db_to': self.db_to}
 
@@ -21,3 +27,7 @@ class Task():
             return "Done"
         else:
             return "Pending"
+
+    def run(self):
+        time.sleep(10)        
+        return 'OK: %s -> %s' % (self.db_from, self.db_to)
