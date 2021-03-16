dbf7580d5d67170d8c67854a1137547d16663f6f
Julien Acroute <julien.acroute@camptocamp.com>
2021-03-16 22:33:52 +0100

    Load svg and animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+/*
+ * Load database definition
+ */
+
+var Httpreq = new XMLHttpRequest(); // a new request
+Httpreq.open("GET", "/api/task/", false);
+Httpreq.send(null);
+dbs = JSON.parse(Httpreq.responseText);
+console.log(dbs);
+
+var rectangle = new Rectangle(new Point(50, 50), new Point(150, 100));
+var path = new Path.Rectangle(rectangle);
+path.fillColor = '#e9e9ff';
+
+var db;
+
+project.importSVG('assets/img/db.svg', function(item, raw) {
+    var db_icon = new Path();
+    item.position = new Point(100, 100);
+    db = item
+    /*db_icon.position = new Point(100, 100);
+    db_icon.add(item);
+    db_icon.position = new Point(100, 100);*/
+    console.log("SVG loaded");
+    });
+
+function onFrame(event) {
+    if (db === undefined) {
+        console.log("SVG not loaded");
+    } else {
+        db.position += new Point(1, 1);
+    }
+}
+
 // Create a Paper.js Path to draw a line into it:
 var path = new Path();
 // Give the stroke a color
