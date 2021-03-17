f8c7537e8723fc8b8378abe55b9f2235ce021b7a
Julien Acroute <julien.acroute@camptocamp.com>
2021-03-17 21:39:20 +0100

    scale and align db

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,38 +7,30 @@ Httpreq.open("GET", "/api/database/", false);
 Httpreq.send(null);
 dbs_json = JSON.parse(Httpreq.responseText);
 var dbs = {}
-
 for (var i = 0; i < dbs_json.length; i++)
     dbs[dbs_json[i].id] = dbs_json[i]
 
-var selected_db;
-var moving_icon;
-var rectangle = new Rectangle(new Point(50, 50), new Point(150, 100));
-var path = new Path.Rectangle(rectangle);
-path.fillColor = '#e9e9ff';
-
-var db_icon;
-
+/*
+ * Load DB Icon and draw DBs
+ */
 project.importSVG('assets/img/db.svg', function(item, raw) {
     console.log("SVG loaded");
-    var db_position = new Point(20,50);
+    var db_position = new Point(100,100);
+    item.scale(2.5);
     
     for (var id in dbs) {
         item.position = db_position;
         dbs[id]['item'] = item;
         item = item.clone();
-        db_position += new Point(170,50);
+        db_position += new Point(300,0);
     }
     item.remove()
 });
-
-function onFrame(event) {
-    if (db_icon === undefined) {
-        console.log("SVG not loaded");
-    } else {
-        db_icon.position += new Point(1, 1);
-    }
-}
+/*
+ * Variable for drag and drop db
+ */
+var selected_db;
+var moving_icon;
 
 
 function onMouseDown(event) {
@@ -59,15 +51,3 @@ function onMouseDrag(event){
         moving_icon.position = event.point
     }
 }
-
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
-
