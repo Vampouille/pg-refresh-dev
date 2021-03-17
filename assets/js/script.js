0f41bb5afaf9bd2865b1d533532220e519f462ac
Julien Acroute <julien.acroute@camptocamp.com>
2021-03-17 23:02:51 +0100

    Add label and animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,16 @@ project.importSVG('assets/img/db.svg', function(item, raw) {
     for (var id in dbs) {
         item.position = db_position;
         dbs[id]['item'] = item;
+        label = new PointText({
+            content: id,
+            fontSize: "2em",
+            position: item.bounds.bottomCenter + new Point(0, 20),
+        });
+        debug = new Path(new Rectangle(item.bounds));
+        debug.strokeColor = "#FF0000";
+        debug.strokeColor = "#FF0000";
+        debug.selected = true;
+        //
         item = item.clone();
         db_position += new Point(300,0);
     }
@@ -30,8 +40,9 @@ project.importSVG('assets/img/db.svg', function(item, raw) {
  * Variable for drag and drop db
  */
 var selected_db;
+var drop_db;
 var moving_icon;
-
+var drop_icon;
 
 function onMouseDown(event) {
     selected_db = undefined;
@@ -40,6 +51,7 @@ function onMouseDown(event) {
             selected_db = id;
             moving_icon = dbs[id]['item'].clone();
             moving_icon.strokeColor = '#FF0000';
+            moving_icon.fillColor = '#FF0000';
             break;
         }
     }
@@ -49,5 +61,40 @@ function onMouseDown(event) {
 function onMouseDrag(event){
     if(moving_icon != undefined){
         moving_icon.position = event.point
+    }
+}
+
+function onMouseUp(event){
+    if(moving_icon != undefined){
+        drop_id = undefined;
+        for (var id in dbs) {
+            if(id != selected_db && dbs[id]['item'].bounds.intersects(moving_icon.bounds)){
+                drop_db = id;
+                console.log("Copy " + selected_db + " --> " + drop_db);
+            } 
+        }
+        drop_icon = moving_icon;
+        moving_icon = undefined;
+    }
+}
+
+function onFrame(){
+    if(drop_icon != undefined){
+        var dest;
+        if(drop_db != undefined){
+            console.log("moving to dest");
+            dest = dbs[drop_db]['item'].bounds.center;
+        } else {
+            console.log("moving to src");
+            dest = dbs[selected_db]['item'].bounds.center;
+        }
+        var move = dest - drop_icon.bounds.center;
+        if(move.length <= 1){
+            drop_icon.remove();
+            drop_icon = undefined;
+        } else {
+            move.length = Math.ceil(move.length / 10);
+            drop_icon.position += move;
+        }
     }
 }
